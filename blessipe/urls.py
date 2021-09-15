0af8d40739d3eac6d27cb115f5b94a151aab118a
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-09-15 11:56:43 -0500

    Can view all recipes

--- blessipe/urls.py
+++ blessipe/urls.py
@@ -18,9 +18,14 @@ from django.urls import path
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
-from blessipeapi.views import register_user, login_user
+from rest_framework import routers
+from blessipeapi.views import register_user, login_user, RecipeView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'recipes', RecipeView, 'recipe'),
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
