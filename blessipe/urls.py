b89f8bf4c38bf9eee415958b8a2aa97b2beeb453
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-09-14 23:02:01 -0500

    Fixed admin panel

--- blessipe/urls.py
+++ blessipe/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.conf.urls import include
+from django.urls import path
+from blessipeapi.views import register_user, login_user
 
 urlpatterns = [
+    path('register', register_user),
+    path('login', login_user),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-] 
+]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blessipeapi/admin.py
+++ blessipeapi/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from blessipeapi.models import Traveler, Country, City, Restaurant, Recipe, Keyword, RecipeKeyword, RestaurantKeyword
 
 # Register your models here.
+admin.site.register(Traveler)
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Restaurant)
+admin.site.register(Recipe)
+admin.site.register(Keyword)
+admin.site.register(RecipeKeyword)
+admin.site.register(RestaurantKeyword)
